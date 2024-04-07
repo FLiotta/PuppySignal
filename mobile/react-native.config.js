7694a8160a803fc44aafc172a0c1f89b2b1b98ad
FLiotta <liottafabrizio@gmail.com>
2024-04-07 19:44:17 -0300

    Different flavors config

--- mobile/react-native.config.js
+++ mobile/react-native.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   project: {
     ios: {},
-    android: {},
+    android: {
+      watchModeCommandParams: ['--mode', 'developmentdebug', '--appIdSuffix', 'development'],
+    },
   },
   assets: ['./assets/fonts'],
 };
