51bc5c2e92f16d75b09cbdcda711513c3d06ac37
Fabrizio Liotta <32228156+FLiotta@users.noreply.github.com>
2024-07-12 21:43:33 -0300

    Missing s3_region field

--- backend/server/config.py
+++ backend/server/config.py
@@ -6,6 +6,7 @@ class Settings(BaseSettings):
   aws_application_key_id: str = None
   aws_application_key: str = None
   s3_bucket: str = None
+  s3_region: str = None
 
   redis_host: str = None
   redis_port: str = None
